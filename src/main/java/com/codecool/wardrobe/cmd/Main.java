79eb229cd628dff59abba97ad8b407bf8f44e151
levayb <levayboldizsar@gmail.com>
2019-03-27 09:50:20 +0100

    UserInterface instansiete new user implemented.

--- src/main/java/com/codecool/wardrobe/cmd/Main.java
+++ src/main/java/com/codecool/wardrobe/cmd/Main.java
@@ -1,4 +1,7 @@
 package com.codecool.wardrobe.cmd;
 
 public final class Main {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+    }
 }
--- src/main/java/com/codecool/wardrobe/cmd/UserInterface.java
+++ src/main/java/com/codecool/wardrobe/cmd/UserInterface.java
@@ -1,7 +1,22 @@
 package com.codecool.wardrobe.cmd;
 
+import com.codecool.wardrobe.api.User;
+
+import java.util.Scanner;
+
 public class UserInterface {
+
+    Scanner scanner = new Scanner(System.in);
+
     public UserInterface() {
     }
+
+    public void makeUser() {
+        String USername = scanner.nextLine();
+        System.out.println("Enter Username");
+        User user = new User(USername);
+    }
+
+
 }
 
